55d86a41c416c58f953da3f2aa3c42bc483a1961
gongfei.chen <gongfei.chen@17zuoye.com>
2020-12-07 14:26:01 +0800

    调整名称

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-基于vue的dom复制功能，可以将dom复制为png图片到剪贴板
-import copy2clipboard from 'copy2clipboard'
-Vue.use(copy2clipboard)
+### 基于vue的dom复制功能，可以将dom复制为png图片到剪贴板
++ import canvas2clipboard from 'canvas2clipboard'
++ Vue.use(canvas2clipboard)
 
 demo:
 ```
-// <div class="png-wrap" ref="pngWrap">
-//    <img width="600px" :src="require('../assets/shuiku.jpg')" alt="">
-// </div>
-<canvas ref="pngWrap"> ... </canvas>
-<button v-copy2clipboard="copypng()">复制图片</button>
+<div class="png-wrap" ref="pngWrap">
+   <img width="600px" :src="require('../assets/shuiku.jpg')" alt="">
+</div>
+<!-- <canvas ref="pngWrap"> ... </canvas> -->
+<button v-canvas2clipboard="copypng()">复制图片</button>
 
 copypng() {
     return {
@@ -22,4 +22,5 @@ copypng() {
 ```
 
 简单实现，主要是提供思路，目前还存在较严重的兼容问题，以后会逐步提代execCommand
-- npm 不允许发布外部依赖项，所以上述 dome 使用 html2canvas 无法直接注入  小伙伴先生成canvas再使用复制
+- npm 不允许发布外部依赖项，所以上述 dome 使用 html2canvas 无法直接注入  
+- 小伙伴先生成canvas再使用复制
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /**
  * js复制图片到剪贴板
  * 复制到剪贴板图片默认png
- * v-copy2clipboard
+ * v-canvas2clipboard
  */
-// import html2canvas from 'html2canvas';
-class copy2clipboard {
+import html2canvas from 'html2canvas';
+class canvas2clipboard {
     constructor(el, binding, vnode) {
         const { target, scale=1, handler: callback, } = binding.value;
         this.scale = scale;
@@ -18,30 +18,30 @@ class copy2clipboard {
                 })
                 return;
             }
-            // this.dom2Image(vnode.context.$refs[target], (canvas) => {
-            //     this.canvas2blob(canvas, (blob) => {
-            //         this.copy(blob, canvas, (result) => {
-            //             callback(result)
-            //         })
-            //     })
-            // })
-            // npm 不允许发布外部依赖项，此处直接传入canvas
-            this.canvas2blob(vnode.context.$refs[target], (blob) => {
-                this.copy(blob, canvas, (result) => {
-                    callback(result)
+            this.dom2Image(vnode.context.$refs[target], (canvas) => {
+                this.canvas2blob(canvas, (blob) => {
+                    this.copy(blob, canvas, (result) => {
+                        callback(result)
+                    })
                 })
             })
+            // npm 不允许发布外部依赖项，此处直接传入canvas
+            // this.canvas2blob(vnode.context.$refs[target], (blob) => {
+            //     this.copy(blob, canvas, (result) => {
+            //         callback(result)
+            //     })
+            // })
         })
     }
     // dom 转 canvas
-    // dom2Image(dom, done) {
-    //     html2canvas(dom, {
-    //         useCORS: true,
-    //         scale: this.scale || 1,
-    //     }).then(canvas => {
-    //         done(canvas);
-    //     });
-    // }
+    dom2Image(dom, done) {
+        html2canvas(dom, {
+            useCORS: true,
+            scale: this.scale || 1,
+        }).then(canvas => {
+            done(canvas);
+        });
+    }
     // canvas 转 blob
     canvas2blob(canvas, done) {
         canvas.toBlob( blob => {
@@ -75,7 +75,7 @@ export default {
     install: (Vue) => {
         Vue.directive('copy2clipboard', {
             bind: function (el, binding, vnode) {
-                new copy2clipboard(el, binding, vnode)
+                new canvas2clipboard(el, binding, vnode)
             }
         });
     }
